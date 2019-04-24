9f5660d6b8d4eb3370d3c321610a2b9c8add5cc7
hupntech <hupntech@gmail.com>
2019-04-24 15:22:57 -0700

    // git init bckc bootstrap  project \\

---
+++ assets/jslibs/carol-popup.js
@@ -0,0 +1,84 @@
+$("#btn-popup-carol").magnificPopup({
+    items: [
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514666/BCKC/Carol/Carol--026-resized.jpg",
+            title: "Carol Gallery Image 1"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514664/BCKC/Carol/Carol--023-resized.jpg",
+            title: "Carol Gallery Image 2"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514664/BCKC/Carol/Carol--018-resized.jpg",
+            title: "Carol Gallery Image 3"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514664/BCKC/Carol/Carol--024-resized.jpg",
+            title: "Carol Gallery Image 4"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514664/BCKC/Carol/Carol--025-resize.jpg",
+            title: "Carol Gallery Image 5"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514664/BCKC/Carol/Carol--021-resized.jpg",
+            title: "Carol Gallery Image 6"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514663/BCKC/Carol/Carol--019-resized.jpg",
+            title: "Carol Gallery Image 7"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514662/BCKC/Carol/Carol--011-resized_Bonus.jpg",
+            title: "Carol Gallery Image 8"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514662/BCKC/Carol/Carol--017-resized.jpg",
+            title: "Carol Gallery Image 9"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514662/BCKC/Carol/Carol-016-resized.jpg",
+            title: "Carol Gallery Image 10"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514662/BCKC/Carol/Carol--009-resized_Bonus.jpg",
+            title: "Carol Gallery Image 11"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514661/BCKC/Carol/Carol--010-resized.jpg",
+            title: "Carol Gallery Image 12"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514661/BCKC/Carol/Carol--008-resized.jpg",
+            title: "Carol Gallery Image 13"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514660/BCKC/Carol/Carol--012-resized.jpg",
+            title: "Carol Gallery Image 14"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514660/BCKC/Carol/Carol--014-resize.jpg",
+            title: "Carol Gallery Image 15"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514658/BCKC/Carol/Carol--006-resized.jpg",
+            title: "Carol Gallery Image 16"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514657/BCKC/Carol/Carol--004-resized.jpg",
+            title: "Carol Gallery Image 17"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514657/BCKC/Carol/Carol--005-resized.jpg",
+            title: "Carol Gallery Image 18"
+        },
+        {
+            src: "https://res.cloudinary.com/hsi/image/upload/v1554514657/BCKC/Carol/Carol--002-resize.jpg",
+            title: "Carol Gallery Image 19"
+        },
+    ],
+    gallery: {
+        enabled: true
+    },
+    type: "image" // this is a default type
+});
